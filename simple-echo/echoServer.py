57b384b613c1dcf9b910805246d9bd8fec271f5e
felix <fxrmillan13@outlook.com>
2023-03-29 01:12:26 -0500

    added fork

--- simple-echo/echoServer.py
+++ simple-echo/echoServer.py
@@ -2,7 +2,7 @@
 
 # Echo server program
 
-import socket, sys, re
+import socket, sys, re,threading
 import os
 from os import read, write
 sys.path.append("../lib")       # for params
@@ -33,30 +33,35 @@ s.bind((listenAddr, listenPort))
 s.listen(1)              # allow only one outstanding request
 # s is a factory for connected sockets
 
-conn, addr = s.accept()  # wait until incoming connection request (and accept it)
-counter =0
-namesAndSizes =[]
-tarFile = 0
-print('Connected by', addr)
-while 1:
-    data = conn.recv(1024).decode()
-    if len(data) == 0:
-        print("Zero length read, nothing to send, terminating")
-        break
-    sendMsg = str("Error").encode()
-    if counter == 0:
-        namesAndSizes = getNamesAndSizes(data)
-        counter += 1
-        sendMsg = str("recived").encode()
-    elif counter > 0:
-        tarFile = data
-    print("Received '%s', sending '%s'" % (data, sendMsg.decode()))
-    while len(sendMsg):
-        bytesSent = conn.send(sendMsg)
-        sendMsg = sendMsg[bytesSent:0]
-conn.shutdown(socket.SHUT_WR)
-conn.close()
-print(namesAndSizes)
-print(tarFile)
+  # wait until incoming connection request (and accept it)
+def server(conn,addr):
+    print('Connected by', addr)
+    counter =0
+    namesAndSizes =[]
+    tarFile = 0
+    print('Connected by', addr)
+    while 1:
+        data = conn.recv(1024).decode()
+        if len(data) == 0:
+            print("Zero length read, nothing to send, terminating")
+            break
+        sendMsg = str("Error").encode()
+        if counter == 0:
+            namesAndSizes = getNamesAndSizes(data)
+            counter += 1
+            sendMsg = str("recived").encode()
+        elif counter > 0:
+            tarFile = data
+        print("Received '%s', sending '%s'" % (data, sendMsg.decode()))
+        while len(sendMsg):
+            bytesSent = conn.send(sendMsg)
+            sendMsg = sendMsg[bytesSent:0]
+    conn.shutdown(socket.SHUT_WR)
+    conn.close()
+    print(namesAndSizes)
+    print(tarFile)
 
-
+while True:
+    conn, addr = s.accept()  # wait until incoming connection request (and accept it)
+    t = threading.Thread(server(conn, addr))
+    t.start()
